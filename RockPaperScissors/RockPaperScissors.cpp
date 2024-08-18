985dfb16d93e3eebd8b827e2f0e20b8929a720a7
ebruh <ebruosman68@gmail.com>
2024-08-18 11:15:19 +0200

    some changes. Improve the code

--- RockPaperScissors/RockPaperScissors.cpp
+++ RockPaperScissors/RockPaperScissors.cpp
@@ -1,105 +1,98 @@
 // RockPaperScissors.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-#include<cstdlib>
-#include<ctime>
+#include <cstdlib>
+#include <ctime>
 #include <string>
 
+const std::string ROCK = "Rock";
+const std::string PAPER = "Paper";
+const std::string SCISSORS = "Scissors";
 
 int generateRandomNum() {
-    srand(static_cast<unsigned>(time(nullptr)));
-    int computerChoice = rand() % 3;
-    return computerChoice;
+    return rand() % 3;
 }
 
-std::string computerChoice() {
-
-    std::string choice = "";
-
-    switch (generateRandomNum())
-    {
+std::string getComputerChoice() {
+    switch (generateRandomNum()) {
     case 0:
-        choice += "Rock";
-        break;
+        return ROCK;
     case 1:
-        choice += "Paper";
-        break;
+        return PAPER;
     case 2:
-        choice += "Scissors";
-        break;
+        return SCISSORS;
     default:
-        break;
+        std::cerr << "Error: Invalid random number generated." << std::endl;
+        std::exit(EXIT_FAILURE);  // Terminate the program with an error status
     }
-
-    return choice;
 }
 
-void game(std::string usersChoice,int* userScore, int *computerScore = 0) {
-    
-    std::string compChoice = computerChoice();
+std::string getUserChoice() {
+    std::string choice;
+    std::cout << "Enter your choice (r/R/Rock, p/P/Paper, s/S/Scissors): ";
+    std::cin >> choice;
 
+    if (choice == "r" || choice == "R" || choice == ROCK || choice == "rock") {
+        return ROCK;
+    }
+    else if (choice == "p" || choice == "P" || choice == PAPER || choice == "paper") {
+        return PAPER;
+    }
+    else if (choice == "s" || choice == "S" || choice == SCISSORS || choice == "scissors") {
+        return SCISSORS;
+    }
+    else {
+        return "";
+    }
+}
 
-    if ((usersChoice == "Rock" && compChoice == "Scissors") ||
-        (usersChoice == "Paper" && compChoice == "Rock") ||
-        (usersChoice == "Scissors" && compChoice == "Paper"))
-    {
-        std::cout << "Congratulations! \nYou win this round! <3";
+void playGame(const std::string& userChoice, int* userScore, int* computerScore) {
+    std::string compChoice = getComputerChoice();
+    std::cout << "Computer choice: " << compChoice << std::endl;
+
+    if ((userChoice == ROCK && compChoice == SCISSORS) ||
+        (userChoice == PAPER && compChoice == ROCK) ||
+        (userChoice == SCISSORS && compChoice == PAPER)) {
+        std::cout << "Congratulations! You win this round! <3\n";
         (*userScore)++;
     }
-    else if((usersChoice == "Scissors" && compChoice == "Rock") ||
-        (usersChoice == "Paper" && compChoice == "Scissors") ||
-        (usersChoice == "Rock" && compChoice == "Paper"))
-    {
-        std::cout << "You lose this round! :(";
+    else if ((userChoice == SCISSORS && compChoice == ROCK) ||
+        (userChoice == PAPER && compChoice == SCISSORS) ||
+        (userChoice == ROCK && compChoice == PAPER)) {
+        std::cout << "You lose this round! :(\n";
         (*computerScore)++;
     }
-    else if (usersChoice == compChoice) {
-        std::cout << "No points";
+    else if (userChoice == compChoice) {
+        std::cout << "It's a tie! No points.\n";
+    }
+    else {
+        std::cout << "Invalid choice. Try again.\n";
     }
 
-    std::cout << std::endl << "Your Score " << "| " << "Computer Score ";
-    std::cout << std::endl << "\t" << *userScore << "  -  " << *computerScore;
+    std::cout << "Your Score: " << *userScore << " | Computer Score: " << *computerScore << std::endl;
 }
 
-int main()
-{
-
-    /*
+int main() {
     srand(static_cast<unsigned>(time(nullptr)));
-    int computerChoice = rand() % 4;
-    std::cout << computerChoice;
-    std::cout << " \n";
-    */
-
-    std::string answer;
 
     int userScore = 0, computerScore = 0;
+    std::string answer = "yes";
 
-    while (answer != "No")
-    {
-        std::string usersChoice;
-        std::cout << "Enter your choice: ";
-        std::cin >> usersChoice;
+    while (answer != "no" && answer != "No") {
+        std::string userChoice = getUserChoice();
 
+        if (!userChoice.empty()) {
+            playGame(userChoice, &userScore, &computerScore);
+        }
+        else {
+            std::cout << "You chose an invalid option. Please try again.\n";
+        }
 
-
-        std::cout << std::endl;
-
-        //std::cout << generateRandomNum();
-        //std::cout << "\n";
-
-        std::cout << "Computer choice: " + computerChoice() << std::endl;
-
-        game(usersChoice, &userScore, &computerScore);
-
-        std::cout << std::endl;
-
-        std::cout << "Do you want to play again? - Yes/No " << std::endl;
+        std::cout << "Do you want to play again? (yes/No): ";
         std::cin >> answer;
-
     }
 
-    
-}
+    std::cout << "Thanks for playing! Final Scores -> You: " << userScore << " | Computer: " << computerScore << std::endl;
 
+    return 0;
+}
